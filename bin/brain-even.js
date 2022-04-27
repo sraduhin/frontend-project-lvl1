b74c57a1679959fa2eff7dc010300cc7b85c8a7a
sraduhin <22dollars@mail.ru>
2022-04-27 16:09:54 +0300

    fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,17 +1,49 @@
-import greedingByName from '../src/cli.js';
 import readlineSync from 'readline-sync';
 
-greedingByName();
+const greedingByName = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have you name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+}
 
-const correctAnswer = (answer, number) => {
-  return (number % 2 //finish here
-const evenGame = () => {
-  const RandomNumber = Math.ceil(Math.random() * 100);// get random number
+// greedingByName();
+
+const correctAnswer = (number) => {
+  return (number % 2 === 0) ? 'yes' : 'no';
+}
+
+const isCorrect = (answer, number) => {
+  return (answer === correctAnswer(number)) ? true : false;
+}
+
+const startRound = () => {
+  const randomNumber = Math.ceil(Math.random() * 100);// get random number
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log(`Question: ${RandomNumber}`);
+  console.log(`Question: ${randomNumber}`);
   const answer = readlineSync.question('Your answer: ');// asking for an answer
+  if (isCorrect(answer, randomNumber)) {
+    console.log('Correct!');
+    return true;
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer(randomNumber)}'`);
+    return false;
+  }
+}
+const evenGame = () => {
+  const name = greedingByName();
+  const rounds = 3;
+  let i = 0;
+  while (i < rounds) {
+    if (startRound()) {
+      i++;
+    } else {
+      console.log(`Let's try again, ${name}!`);
+    }
+  }
+  console.log(`Congratulations, ${name}`);
   
-
+  return;
 }
 
 evenGame();
