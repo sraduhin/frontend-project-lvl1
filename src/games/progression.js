67d77b01b1f9e3f2539bb069b75fa60b4536f333
Sergey Radyukhin <22dollars@mail.ru>
2022-05-08 18:16:45 +0300

    fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,19 +5,19 @@ import genRandomNum from '../randomNumber.js';
 const descPurpose = 'What number is missing in the progression?';
 
 const makeRow = (start, increment, length, hidden) => {
-  let row = '';
+  let row = [];
   let hiddenIndex = 0;
   for (let i = 0; i < length; i += 1) {
     if (i === hidden) {
-      row = `${row} ..`;
+      row.push('..');
       hiddenIndex = start;
       start += increment;
     } else {
-      row = `${row}  ${start}`;
+      row.push(start);
       start += increment;
     }
   }
-  return [row, hiddenIndex];
+  return [row.join(' '), hiddenIndex];
 };
 const genRound = () => {
   const startProgression = genRandomNum(0, 100);
