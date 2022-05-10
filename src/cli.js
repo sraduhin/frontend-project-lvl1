a22186f887660d2dbc277e95b73df00e12729a2c
Sergey Radyukhin <22dollars@mail.ru>
2022-05-10 14:52:50 +0300

    refactoring

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 const greetUser = () => {
-  console.log('brain-games');
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
